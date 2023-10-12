6e915568bb3e0ffa9ad0d6bd3bd089a16f325b5c
ninja11 <56148354+ninja1911@users.noreply.github.com>
2023-10-12 12:05:43 -0700

    Update Stream_lit_5graphs.py

--- Stream_lit_5graphs.py
+++ Stream_lit_5graphs.py
@@ -5,18 +5,19 @@ import streamlit as st
 st.title("Selectbox")
 graphs = ["Linechart","BarChart","Scatterplot","BoxPlot","PieChart"]
 graph_selected=st.selectbox("Choose a graph to visualize",options=graphs)
+df=pd.read_csv(r"Fish.csv")
+data=df.groupby('Species').agg({"Weight": ["mean","min","max"]})
+# rename columns
+data.columns = ['weight_mean', 'weight_min', 'weight_max']
+# reset index to get grouped columns back
+data = data.reset_index()
+data=data.sort_values(by=['weight_mean'], ascending=True)
+
 if graph_selected=="Linechart":
-    df=pd.read_csv(r"Fish.csv")
-    data=df.groupby('Species').agg({"Weight": ["mean","min","max"]})
-    # rename columns
-    data.columns = ['weight_mean', 'weight_min', 'weight_max']
-    # reset index to get grouped columns back
-    data = data.reset_index()
-    data=data.sort_values(by=['weight_mean'], ascending=True)
+
     plt.plot(data['Species'], data['weight_mean'], label ='weight_mean', marker='o')
     plt.plot(data['Species'], data['weight_min'], label ='weight_min', marker='o')
     plt.plot(data['Species'], data['weight_max'], label ='weight_max', marker='o')
-    
     plt.title('Max/Mean/Min weight across species in increasing order', fontsize=14)
     plt.xlabel('Species', fontsize=14)
     plt.ylabel('Weight', fontsize=14)
@@ -73,3 +74,27 @@ elif graph_selected=="BarChart":
     #inserting the header for line chart
     st.subheader("Bar Chart:")
     st.pyplot(Bar_Chart)
+elif graph_selected=="Scatterplot":
+    colors = ['b', 'y', 'm', 'r']
+    perch_data=df[df['Species']=='Perch']
+    pike_data=df[df['Species']=='Pike']
+    
+    lo = plt.scatter(perch_data['Height'], perch_data['Weight'], marker='x', color=colors[0])
+    ll = plt.scatter(whitefish_data['Height'], pike_data['Weight'], marker='o', color=colors[1])
+    
+    plt.title("Height and Weight Scatter plot of Perch vs Pike Species")
+    plt.xlabel("Height")
+    plt.ylabel("Weight")
+    plt.legend((lo, ll),
+               ('Perch', 'Pike'),
+               scatterpoints=1,
+               loc='upper left',
+               ncol=3,
+               fontsize=8)
+    
+    plt.show()
+    Scatter_plot=plt.show()
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    #inserting the header for Scatter plot
+    st.subheader("Scatter plot:")
+    st.pyplot(Scatter_plot)
