13567dc9c1832e0fa8c33254176c56c86afcb503
ninja11 <56148354+ninja1911@users.noreply.github.com>
2023-10-12 12:59:09 -0700

    Update Stream_lit_5graphs.py

--- Stream_lit_5graphs.py
+++ Stream_lit_5graphs.py
@@ -39,7 +39,11 @@ if graph_selected=="Linechart":
     #inserting the header for line chart
     st.subheader("Line Chart:")
     st.pyplot(Line_chart)
-    st.write('The transformed data used to populate the chart: ')
+    st.write('A few basic insights of the given graph:
+•	From the data we can infer that smelt is least weighed and Pike is the heaviest of all the fishes.
+•	Also for Pike and Perch the min and max values are spread out a lot more than the rest whereas for Smelt all 3 values are around a close range thus making the mean of this species much more reliable.
+•	Also from the data we can see that the min value of roach is 0 making it an anomaly or out layer.
+ ')
 elif graph_selected=="BarChart":
     df=pd.read_csv(r"Fish.csv")
     data=df.groupby('Species').agg({"Weight": ["mean","min","max"]})
